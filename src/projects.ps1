a6b60e9b26aad26085d098928b51a10b20f5b777
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2022-07-27 18:16:46 -0300

    added paramter

--- src/projects.ps1
+++ src/projects.ps1
@@ -1,8 +1,17 @@
 function Add-Project {
   [CmdletBinding()]
   param (
-    [Parameter(Mandatory=$true)]
-    [string]$Param
+    [Parameter(Mandatory=$true,Position=0,HelpMessage="Syntax: <DIRECTORY_NAME>")]
+    [ValidateNotNullOrEmpty()]
+    [string]$name="",
+
+    [Parameter(Mandatory=$false,HelpMessage="")]
+    [ValidateSet("null")]
+    [string]$framework,
+
+    [Parameter(Mandatory=$false,HelpMessage="")]
+    [ValidateSet("null")]
+    [string]$extension
   )
 
   begin {
