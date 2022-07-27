bb509c649a1cec9cd20966b59371b1b4b2932161
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2022-07-27 19:27:22 -0300

    changed scope

---
+++ test/add.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true,Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [string]$name="",
+
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("null")]
+    [string]$framework,
+
+    [Parameter(Mandatory=$false)]
+    [ValidateSet("null")]
+    [string]$extension
+  )
+
+  $path = ((Get-Content "$PSScriptRoot/../.config") -split ":")[1]
+  try {
+    New-Item "$path/$name" -ItemType Directory 
+  } catch {
+    Write-Error "The project '$name' already exist"    
+  }
