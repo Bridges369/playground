e55efed4d361d23e5eb0666d7e8a0ee2af187b94
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2022-08-05 22:23:22 -0300

    updated parameters for a concordance; added help message and alias for each one

--- src/Add-Project.ps1
+++ src/Add-Project.ps1
@@ -1,47 +1,65 @@
 [CmdletBinding()]
 param (
-  [Parameter(Mandatory=$true,Position=0)]
+  [Parameter(
+    Mandatory=$true,Position=0,
+    HelpMessage="Enter the name of project."
+  )]
   [ValidateNotNullOrEmpty()]
-  [string]$name="",
+  [Alias("n")]
+  [string]$Name,
 
-  [Parameter(Mandatory=$false)]
+  [Parameter(
+    Mandatory=$false,
+    HelpMessage="Enter the framework used."
+  )]
+  [ValidateNotNullOrEmpty()]
   #[ValidateSet("null")]
-  [string]$framework,
+  [Alias("f")]
+  [string]$Framework,
 
-  [Parameter(Mandatory=$false)]
+  [Parameter(
+    Mandatory=$false,
+    HelpMessage="Enter the extension of main file."
+  )]
+  [ValidateNotNullOrEmpty()]
   #[ValidateSet("null")]
-  [string]$extension,
+  [Alias("e")]
+  [string]$Extension,
 
-  [Parameter(Mandatory=$false)]
-  [string]$params
+  [Parameter(
+    Mandatory=$false,
+    HelpMessage="Enter additional params for the framework used."
+  )]
+  [Alias("p","params")]
+  [string]$Additional_params
 )
 
 $path = ((Get-Content "$PSScriptRoot/../.config") -split ":")[1]
 
-if ($framework -ne "") {
-  switch ( $framework ){
+if ($Framework) {
+  switch ( $Framework ){
     "rust" {
-      cargo new $name $params
+      cargo new $Name $Additional_params
     }
     default { 
       Write-Error "This framework doesn't seem to be installed."
       exit
     }
   }
-} elseif ($extension -ne "") {
+} elseif ($Extension) {
   try {
-    New-Item "$path/$name" -ItemType Directory -ErrorAction Stop
+    New-Item "$path/$Name" -ItemType Directory -ErrorAction Stop
   } catch {
-    Write-Error "The project '$name' already exist"
+    Write-Error "The project '$Name' already exist"
     exit
   }
-  New-Item "$path/$name/main.$extension" -ItemType File
-  Set-Location "$path/$name"
+  New-Item "$path/$Name/main.$Extension" -ItemType File
+  Set-Location "$path/$Name"
 } else {
   try {
-    New-Item "$path/$name" -ItemType Directory -ErrorAction Stop
+    New-Item "$path/$Name" -ItemType Directory -ErrorAction Stop
   } catch {
-    Write-Error "The project '$name' already exist"
+    Write-Error "The project '$Name' already exist"
     exit
   }
 }
